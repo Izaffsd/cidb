8323ec32faeda5e2d0cce3d395560b41ac55acae
ATHILIASOFIA\PC <work.athiliasofia@gmail.com>
2025-05-29 21:09:59 +0800

    Create Page card.php

---
+++ card.php
@@ -0,0 +1,104 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="icon" type="image/png" sizes="32x32" href="icon.png">
+
+
+    <title>Mewah Services</title>
+    <link rel="stylesheet" href="./assests/style.css">
+
+    <!-- RemixIcons CDN -->
+
+    <script src="https://kit.fontawesome.com/0ede200358.js" crossorigin="anonymous" defer></script>
+
+    <link href="
+    https://cdn.jsdelivr.net/npm/remixicon@4.3.0/fonts/remixicon.min.css
+    " rel="stylesheet">
+    </head>
+<body>
+
+    <?php include 'header.php' ?>
+
+<!-- header ends -->
+
+<?php include 'home-sect.php' ?>
+
+
+<section class="cidb-info-section">
+    <div class="container">
+        <div class="text-content">
+            <h2>Adakah Kad CIDB dan Kad Hijau Berbeza?</h2>
+            <p><strong>Ya.</strong> Kad CIDB dan Kad Hijau adalah kad yang SAMA. Kad ini dikeluarkan oleh <strong>Lembaga Pembangunan Industri Pembinaan Malaysia (CIDB)</strong> kepada individu dalam industri pembinaan di Malaysia.</p>
+            <p>Kad ini membuktikan bahawa pemiliknya mempunyai kelayakan, kebolehan, dan dibenarkan untuk menjalankan kerja-kerja pembinaan. Berikut adalah kelebihan yang dimiliki pemegang Kad CIDB:</p>
+            <ul>
+                <li><span>Kredibiliti:</span> Menunjukkan kelayakan dan kebolehan untuk menjalankan tugas berkaitan pembinaan.</li>
+                <li><span>Ilmu dan Pengalaman:</span> Diperolehi melalui kursus asas seperti NIOSH atau SICW. Di dalam kursus ini, peserta diterapkan dengan ciri-ciri keselamatan semasa di tapak binaan. Melalui kursus ini juga, mereka dapat memahami risiko dan langkah-langkah yang perlu diambil semasa berada di tapak binaan.</li>
+                <li><span>Peluang Pekerjaan:</span> Pemegang Kad CIDB mempunyai peluang kerja lebih besar. Kebanyakan majikan mensyaratkan pekerja mereka, samada mahir atau separuh mahir, mempunyai kad ini. Ada juga sesetengah majikan menanggung kos pembiayaan pekerja mereka untuk mendapatkan kad berkenaan.</li>
+                <li><span>Kursus Latihan:</span> CIDB menawarkan kursus untuk meningkatkan kemahiran. Kursus ini dijalankan sama ada secara bersemuka atau atas talian. Kursus ini juga melayakkan peserta mendapatkan mata CCD bagi individu dan organisasi yang diwakili.</li>
+                <li><span>Perlindungan Insurans:</span> Pemegang kad dilindungi oleh insurans kemalangan kerja 24 jam serta insurans nyawa. Insurans kemalangan kerja memberi perlindungan kepada pemegang kad CIDB terhadap kecederaan akibat kemalangan di tempat kerja.</li>
+            </ul>
+            <p><span>Perlindungan Insurans:</span> Pemegang kad dilindungi insurans kerja yang meliputi kecederaan, pampasan kematian akibat kemalangan di tempat kerja, dan insurans nyawa. Ini memastikan pemegang kad serta warisnya dilindungi.</p>
+
+            <!-- New Paragraphs -->
+            <p>Insurans kemalangan kerja memberikan perlindungan terhadap kecederaan atau kematian yang disebabkan oleh kemalangan semasa menjalankan tugas-tugas di tempat kerja. Manfaat yang diberikan termasuk pembayaran gantirugi perubatan, gantirugi pendapatan, dan pampasan kematian. Insurans nyawa pula memberikan perlindungan terhadap kematian yang disebabkan oleh sebab-sebab lain selain kemalangan kerja. Manfaat yang diberikan termasuk pembayaran pampasan kematian kepada waris-waris yang ditetapkan.</p>
+
+            <p><strong>Siapa yang mengeluarkan kad CIDB?</strong></p>
+            <p>Kad CIDB dikeluarkan oleh Lembaga Pembangunan Industri Pembinaan Malaysia atau The Construction Industry Development Board (CIDB). Menurut Akta Lembaga Pembangunan Industri Pembinaan Malaysia 1994 (Akta 520), setiap individu yang terlibat dalam industri pembinaan di Malaysia perlu mempunyai kad pengenalan CIDB yang sah. Jika seorang personel binaan tidak mempunyai kad CIDB yang sah, dia boleh dikenakan hukuman.</p>
+
+            <p>Hukuman bagi personel binaan yang tidak mempunyai kad CIDB termasuk denda tidak melebihi RM50,000 atau penjara tidak melebihi 1 tahun atau kedua-duanya sekali. Selain itu, syarikat-syarikat pembinaan yang mengambil pekerja yang tidak mempunyai kad CIDB juga boleh dikenakan tindakan undang-undang.</p>
+
+                        <p>Jika syarikat didapati menggaji personel binaan yang tidak mempunyai kad CIDB yang sah, syarikat tersebut boleh dikenakan denda tidak melebihi RM250,000 atau penjara tidak melebihi 3 tahun atau kedua-duanya sekali. Oleh itu, adalah penting bagi personel binaan untuk memastikan mereka mempunyai kad CIDB yang sah untuk mengelakkan daripada dikenakan hukuman dan juga untuk memastikan mereka mempunyai kelayakan dan kebolehan untuk menjalankan tugas-tugas di industri pembinaan dengan selamat dan berkesan.</p>
+
+                        <p><strong>Siapa yang layak memiliki kad CIDB?</strong></p>
+                        <p>Individu yang hendak memohon mestilah memenuhi 5 syarat berikut:</p>
+                        <ol class="styled-list">
+                            <li>Memiliki dokumen pengenalan diri seperti:
+                                <ul>
+                                    <li>Kad Pengenalan Malaysia.</li>
+                                    <li>Pasport dengan permit kerja yang sah dari Jabatan Imigresen Malaysia.</li>
+                                    <li>Pas IMM13P.</li>
+                                    <li>Berumur 16 tahun dan ke atas. Personel binaan dilindungi sehingga 70 tahun di bawah Pelan Perlindungan Personel Binaan Berkelompok CIDB.</li>
+
+                                </ul>
+                            </li>
+                            <li>Mempunyai sijil keselamatan pembinaan seperti:
+                                <ul>
+                                    <li>Kursus Induksi Keselamatan dan Kesihatan Pekerja di Tapak Bina (SICW);</li>
+                                    <li>eLearning Kursus Induksi Keselamatan dan Kesihatan Pekerja di Tapak Bina (eSICW);</li>
+                                    <li>eSICW CIDB Holdings (eSICW CIDBH); atau</li>
+                                    <li>Sijil/ pasport keselamatan yang diiktiraf CIDB.</li>
+                                </ul>
+                            </li>
+                            <li>Melaksanakan kerja-kerja pembinaan.</li>
+                            <li>Mematuhi undang-undang dan peraturan dari CIDB.</li>
+                        </ol>
+                        
+
+                        <p><strong>Kesimpulan:</strong></p>
+                        <p>Sebagai kesimpulan, Kad CIDB dan kad hijau adalah merujuk kepada dokumen yang sama. Kad ini juga dikenali sebagai Kad Personel Binaan, Green Card, dan Pass kerja CIDB. Kepada anda yang berkecimpung di dalam bidang binaan disarankan agar memiliki dokumen ini.</p>
+
+                    </div>
+                </div>
+            </section>
+                <!-- WhatsApp Floating Button -->
+                <a href="http://www.watsap.my/601126165376/CIDBKadhijauwebsite" class="whatsapp-float" target="_blank" rel="noopener noreferrer" aria-label="WhatsApp Chat">
+                    <div class="whatsapp-btn">
+                        <i class="fab fa-whatsapp"></i> Kami Sedia Membantu Anda!
+                    </div>
+                </a>
+
+
+            <!-- footer section starts -->
+            <?php  include 'footer.php' ?> 
+
+            <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.6.0/jquery.min.js" integrity="sha512-894YE6QWD5I59HgZOGReFYm4dnWc1Qt5NtvYSaNcOP+u1T9qYdvdihz0PPSiiqn/+/3e7Jo4EaG7TubfWGUrMQ==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
+
+            <script src="https://unpkg.com/swiper/swiper-bundle.min.js"></script>
+
+            <script src="./assests/script.js"></script>
+
+            </body>
+            </html>
